692e84772b6b27e0dd06fb92dd50397e7a312484
velfoxx <viliusvainorius@gmail.com>
2022-11-29 17:22:05 +0200

    vr working updated

--- Assets/Scripts/CueHandler.cs
+++ Assets/Scripts/CueHandler.cs
@@ -2,21 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class CueHandler : MonoBehaviour
 {
     public ActionBasedController frontController;
     public ActionBasedController backController;
+    
 
     //kazkas apie button press (nezinau ar pades)
     //https://www.youtube.com/watch?v=NdHGuj-u-kc
-    public InputHelpers.Button button = InputHelpers.Button.None;
+    public InputActionProperty right;
+    public InputActionProperty left;
 
     private Rigidbody cueRB;
 
     private Vector3 cuePos;
     private float lockOffset;
     private Vector3 lockForward;
+    
+
+    //Vector3 frontPos;
+    //Vector3 backPos;
+
+    //GameObject original;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,24 +37,33 @@ public class CueHandler : MonoBehaviour
     void Update()
     {
         UpdateCuePosition();
+        /*frontPos = frontController.transform.position;
+        backPos = backController.transform.position;
+        cuePos = 0.75f * backPos + 0.25f * frontPos;
+        cueRB.MovePosition(cuePos);
+        cueRB.MoveRotation(Quaternion.LookRotation(frontPos - backPos));
+        */
+
     }
 
     void UpdateCuePosition()
-    {/*
+    {
         //kazkoki button reik gaut
         //pabandyt su input helper kazka gal (11 eilute) 
-        var back = StemVR_Controller.Input((int)backController.index);
+        var backRight = right.action.ReadValue<float>();
+        var backLeft = left.action.ReadValue<float>();
+        
+        Vector3 frontPos = frontController.transform.position;
+        Vector3 backPos = backController.transform.position;
 
-        Vector3 frontPos = frontController.transform.position; ;
-        Vector3 backPos = backController.transform.position; ;
 
-        if (back.GetPressDown(StemVR_Controller.ButtonMask.Trigger))//first press of trigger
+        if (backRight == 1  && backLeft != 1)//first press of trigger
         {
             //print("first press");
             lockForward = transform.forward; //gali reik keist priklausomai nuo pivot
             lockOffset = (frontPos - backPos).magnitude;
         }
-        else if(back.GetPressDown(StemVR_Controller.ButtonMask.Trigger))// trigger held down
+        else if(backRight == 1 && backLeft == 1)// trigger held down
         {
             //print("press hold");
             float currOffset = (frontPos - backPos).magnitude;
@@ -58,7 +77,7 @@ public class CueHandler : MonoBehaviour
             cueRB.MoveRotation(Quaternion.LookRotation(frontPos - backPos));
         }
         
-        */
+        
     }
 
     void OnCollisionEnter(Collision collision)
